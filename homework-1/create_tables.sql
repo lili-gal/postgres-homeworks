0cfc9eca59d05459a543c14292d0c0b3fbe3be0a
Лилия Галиуллина <lili_gal@mail.ru>
2023-06-19 15:34:27 +0500

    add main

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id VARCHAR(10) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date DATE NOT NULL,
+	notes text
+);
+CREATE TABLE orders
+(
+	order_id int,
+	customer_id TEXT REFERENCES customers(customer_id),
+	employee_id	int REFERENCES employees(employee_id),
+	order_date DATE NOT NULL,
+	ship_city varchar(50) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+'''Подключение к БД'''
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="Dfcmrf74"
+)
+
+csv_files = {
+    "north_data/customers_data.csv": "customers",
+    "north_data/employees_data.csv": "employees",
+    "north_data/orders_data.csv": "orders"
+}
+for csv_file, table_name in csv_files.items():
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Пропускаем заголовок в файле CSV
+        cursor = conn.cursor()
+        for row in reader:
+            insert_query = f"INSERT INTO {table_name} VALUES ({','.join(['%s']*len(row))})"
+            cursor.execute(insert_query, row)
+        conn.commit()
+
+cursor.close()
+conn.close()
